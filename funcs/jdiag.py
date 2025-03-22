d4c0a5cec69cbd5e4c9ba5c98e9cbe2a60bb46de
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-03-22 10:31:08 -0600

    add jdiag.py

---
+++ funcs/jdiag.py
@@ -0,0 +1,21 @@
+
+def load_jdiag(filename, dataset):
+    from netCDF4 import Dataset
+    dataset = Dataset(filename, "r")
+
+def get_jdiag_metadata(dataset, metadata):
+    metadata["latitude"] = dataset.groups["MetaData"].variables["latitude"][:]
+    metadata["longitude"] = dataset.groups["MetaData"].variables["longitude"][:]
+    metadata["pressure"] = dataset.groups["MetaData"].variables["pressure"][:]
+    metadata["height"] = dataset.groups["MetaData"].variables["height"][:]
+    if "aircraftFlightNumber" in dataset.groups["MetaData"].variables:
+        metadata["aircraftFlightNumber"] = dataset.groups["MetaData"].variables["aircraftFlightNumber"][:]
+        metadata["aircraftFlightPhase"] = dataset.groups["MetaData"].variables["aircraftFlightPhase"][:]
+
+def get_jdiag_data(dataset, varname, data):
+    data["ObsType"] = dataset.groups["ObsType"].variables[varname][:]
+    data["ObsValue"] = dataset.groups["ObsValue"].variables[varname][:]
+    data["QualityMarker"]  = dataset.groups["QualityMarker"].variables[varname][:]
+    data["EffectiveQC0"] = dataset.groups["EffectiveQC0"].variables[varname][:]
+    if varname == "specificHumidity":
+        data["ObsValue"] = data["ObsValue"] * 1000
