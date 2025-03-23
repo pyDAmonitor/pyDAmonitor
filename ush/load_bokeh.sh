c266c9131911edd27789e676ff7b3fb1766fa909
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-03-22 22:24:57 -0400

    use the bokeh Python env by default

---
+++ ush/load_bokeh.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# Check if the script is sourced
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  echo "Usage: source ${0}"
+  exit 1
+fi
+
+### scripts continues here...
+ushdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source ${ushdir}/detect_machine.sh
+
+module purge
+module use ${ushdir}/../modulefiles
+if [[ "${MACHINE}" == "gaea" ]]; then
+  if [[ -d /gpfs/f5 ]]; then
+    module load BOKEH/${MACHINE}C5
+  elif [[ -d /gpfs/f6 ]]; then
+    module load BOKEH/${MACHINE}C6
+  else
+    echo "not supported gaea cluster: ${MACHINE}"
+  fi
+else
+  module load BOKEH/${MACHINE}
+fi
+module list
+
+${ushdir}/init.sh
+nbstripout --install
