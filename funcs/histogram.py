2d3887ead307d3add716f2dd7aebd7911a6c5b52
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-03-24 19:36:38 -0600

    update histogram to have more tuning parameters

--- funcs/histogram.py
+++ funcs/histogram.py
@@ -1,16 +1,20 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.ticker as ticker
 
-def histogram(group, bin_size, xlabel, title, fig_name=None):
-    bins = range( int(group.min()), int(group.max())+bin_size, bin_size)
+def histogram(data, bin_size, n_xticks, xlabel, title, fig_name=None):
+    bins = np.arange( int(data.min()), int(data.max())+bin_size, bin_size)
     plt.figure()
-    plt.hist(group, bins=bin_size, edgecolor='black')
+    plt.hist(data, bins=bins, edgecolor='black')
     plt.xlabel(xlabel)
     plt.ylabel('Frequency')
     plt.title(title)
-    plt.xticks(bins)
-    print(bins)
+    # Set x-axis locator to limit number of ticks
+    plt.gca().xaxis.set_major_locator(ticker.MaxNLocator(nbins=n_xticks))  # Adjust number of labels dynamically
+    #plt.xticks(bins)
+    plt.xticks(rotation=45)
     if fig_name:
         plt.savefig(fig_name, dpi=250, bbox_inches='tight')
     else:
