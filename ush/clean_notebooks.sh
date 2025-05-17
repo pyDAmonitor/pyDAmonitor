772da97c83bb0c9ec84e9a0c7df96cccbacd22a0
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-05-17 12:40:48 -0600

    remove bak.load_eva.sh clean_notebooks.sh

--- ush/clean_notebooks.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#
-run_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd ${run_dir}/..
-git status notebooks | grep "notebook.*ipynb" | sed 's/.*\(notebooks\/.*\)/\1/' | xargs -I {} nbstripout "{}"
