312e2358886551dc9c950c4364f82124c6422f5e
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-05-17 17:33:03 -0400

    move shapes.py one level up

--- DAmonitor/mpas/__init__.py
+++ DAmonitor/mpas/__init__.py
@@ -1,11 +1,7 @@
 from .hslice_contour import hslice_contour
 from .vslice_contour import vslice_contour
-from .shapes import coast_lines, state_lines, county_lines
 
 __all__ = [
     'hslice_contour',
     'vslice_contour',
-    'coast_lines',
-    'state_lines',
-    'county_lines',
 ]
