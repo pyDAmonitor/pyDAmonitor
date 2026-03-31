f62d7ecccdef716e1701ccbf9f5c7bd7494fd68a
guoqing.ge <Guoqing.Ge@noaa.gov>
2026-03-31 10:26:36 -0600

    update detect_machine.sh, load_pyDAmonitor.sh and init.sh

--- ush/init.sh
+++ ush/init.sh
@@ -23,9 +23,9 @@ case ${MACHINE} in
   orion|hercules)
     FIX_RRFS_LOCATION=/work/noaa/zrtrr/FIX_pyDAmonitor
     ;;
-  gaea)
+  gaeac?)
     if [[ -d /gpfs/f5 ]]; then
-      FIX_RRFS_LOCATION=/gpfs/f5/gsl-glo/world-shared/role.rrfsfix/FIX_pyDAmonitor
+      BASEDIR=/to/be/added
     elif [[ -d /gpfs/f6 ]]; then
       FIX_RRFS_LOCATION=/gpfs/f6/bil-fire10-oar/world-shared/FIX_pyDAmonitor
     else
