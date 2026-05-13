8003f41ef32b16548fbea03787085a18a5b9794b
Keenan Eure <keenan.eure@noaa.gov>
2026-05-13 16:52:32 +0000

    Address linter comments

--- scripts/mpasjedi_anldiff_fulldom_pres.py
+++ scripts/mpasjedi_anldiff_fulldom_pres.py
@@ -1,6 +1,6 @@
-#created 12 May 2026 by K. Eure: plots retro-averaged difference of analysis (of 2 retros) at user-specified pressure levels
-#NOTE: this script is quite slow (due to reading in files and interpolation on MPAS), so running this in a batch job is advisable
 #!/usr/bin/env python
+# Created 12 May 2026 by K. Eure: plots retro-averaged difference of analysis (of 2 retros) at user-specified pressure levels
+# Note: this script is quite slow (due to reading in files and interpolation on MPAS), so running this in a batch job is advisable
 from scipy.interpolate import interp1d
 from matplotlib.tri import Triangulation, TriAnalyzer
 import warnings
@@ -37,14 +37,14 @@ nc_keys = ["theta", "qv", "uReconstructZonal"]
 expname = "TMS"  # used in plot and filename for saving
 pressure_levels = [250, 550, 850]  # pressure levels in hPa
 plot_var = "Analysis Difference"
-decimals = 2 # number of decimals to round for text boxes
+decimals = 2  # number of decimals to round for text boxes
 
 # Input configurations
-base_path1 = "/{retropathway}/com/rrfs/{versionnumber}" #pathway to control/conv experiment
-base_path2 = "/{retropathway}/com/rrfs/{versionnumber}" #pathway to exp to compare against base_path1
-jstatic = "invariant.nc" #arbitrary file for grid info
+base_path1 = "/{retropathway}/com/rrfs/{versionnumber}"  # pathway to control/conv experiment
+base_path2 = "/{retropathway}/com/rrfs/{versionnumber}"  # pathway to exp to compare against base_path1
+jstatic = "invariant.nc"  # arbitrary file for grid info
 start_dt = datetime.strptime("YYYYMMDD HH", "%Y%m%d %H")
-end_dt   = datetime.strptime("YYYYMMDD HH", "%Y%m%d %H")
+end_dt = datetime.strptime("YYYYMMDD HH", "%Y%m%d %H")
 subpath1 = "jedivar/det/mpasout.*.nc"
 subpath2 = "jedivar/det/mpasout.*.nc"
 
@@ -83,23 +83,23 @@ for nc_key in nc_keys:
     # Checks settings for nc_key variable
     clean_name, unit_label, clevmax, conv_factor = var_lookup[nc_key]
     print(f"\n--- Processing {clean_name} ---")
-    #initialize fields before hourly loop
+    # initialize fields before hourly loop
     sum_jedi_diff_3d = None
     sum_pres_1_3d = None
-    valid_obs_count = None # Added this to track non-NaN counts per cell
-    count = 0 #resets before every time loop
-    current_dt = start_dt #ensure time is at beginning
+    valid_obs_count = None  # Added this to track non-NaN counts per cell
+    count = 0  # resets before every time loop
+    current_dt = start_dt  # ensure time is at beginning
 
     # Loop over hourly cycles
     while current_dt <= end_dt:
         date_str = current_dt.strftime("%Y%m%d")
         hour_str = current_dt.strftime("%H")
-        #specific filename for this time
+        # specific filename for this time
         fname = current_dt.strftime("mpasout.%Y-%m-%d_%H.00.00.nc")
-        #construct full paths
+        # construct full paths
         file_path1 = os.path.join(base_path1, f"rrfs.{date_str}", hour_str, "jedivar", "det", fname)
         file_path2 = os.path.join(base_path2, f"rrfs.{date_str}", hour_str, "jedivar", "det", fname)
-        ymdh = current_dt.strftime("%Y%m%d%H") #for labeling
+        ymdh = current_dt.strftime("%Y%m%d%H")  # for labeling
         time_label = current_dt.strftime("%m-%d %H")
 
         # Check for missing file, 'continue' goes to next iteration
@@ -108,19 +108,19 @@ for nc_key in nc_keys:
             current_dt += timedelta(hours=1)
             continue
         try:
-            #read mode
-            nc_1 = Dataset(file_path1, mode='r') #cntrl
-            nc_2 = Dataset(file_path2, mode='r') #exp of interest
-            #read in fields and get dimensions
+            # read mode
+            nc_1 = Dataset(file_path1, mode='r')  # cntrl
+            nc_2 = Dataset(file_path2, mode='r')  # exp of interest
+            # read in fields and get dimensions
             jedi_1 = nc_1.variables[nc_key][0, :, :].astype(np.float64)
             jedi_2 = nc_2.variables[nc_key][0, :, :].astype(np.float64)
-            #read in pressure
-            pres_1_raw = (nc_1.variables['pressure_p'][0,:,:] + nc_1['pressure_base'][0,:,:])/100.0
-            pres_2_raw = (nc_2.variables['pressure_p'][0,:,:] + nc_2['pressure_base'][0,:,:])/100.0
-            #fix put in - use surface pressure to mask any locations that have pres_# values below the surface
-            pres_sfc_1 = nc_1.variables['surface_pressure'][0,:]/100
-            pres_sfc_2 = nc_2.variables['surface_pressure'][0,:]/100
-            #screen out these values in each exp's field
+            # read in pressure
+            pres_1_raw = (nc_1.variables['pressure_p'][0, :, :] + nc_1['pressure_base'][0, :, :])/100.0
+            pres_2_raw = (nc_2.variables['pressure_p'][0, :, :] + nc_2['pressure_base'][0, :, :])/100.0
+            # fix put in - use surface pressure to mask any locations that have pres_# values below the surface
+            pres_sfc_1 = nc_1.variables['surface_pressure'][0, :]/100
+            pres_sfc_2 = nc_2.variables['surface_pressure'][0, :]/100
+            # screen out these values in each exp's field
             pres_1 = np.where(pres_1_raw <= pres_sfc_1[:, None], pres_1_raw, np.nan)
             pres_2 = np.where(pres_2_raw <= pres_sfc_2[:, None], pres_2_raw, np.nan)
             # convert from theta to temp when needed
@@ -132,7 +132,7 @@ for nc_key in nc_keys:
             if nc_key == "uReconstructZonal":
                 jedi_1 = np.hypot(nc_1.variables[nc_key][0, :, :].astype(np.float64), nc_1.variables["uReconstructMeridional"][0, :, :].astype(np.float64))
                 jedi_2 = np.hypot(nc_2.variables[nc_key][0, :, :].astype(np.float64), nc_2.variables["uReconstructMeridional"][0, :, :].astype(np.float64))
-            #add same correction to jedi_# that we did to pres_#
+            # add same correction to jedi_# that we did to pres_#
             jedi_1 = np.where(np.isnan(pres_1), np.nan, jedi_1)
             jedi_2 = np.where(np.isnan(pres_2), np.nan, jedi_2)
             # convert to correct units
@@ -149,7 +149,8 @@ for nc_key in nc_keys:
             count += 1
             print(f"{ymdh} collected")
             # close files
-            nc_1.close(); nc_2.close()
+            nc_1.close();
+            nc_2.close()
         except Exception as e:
             print(f"Error: {e}")
         current_dt += timedelta(hours=1)
@@ -234,4 +235,3 @@ for nc_key in nc_keys:
 
     else:
         print(f"No data collected for {nc_key}.")
-
