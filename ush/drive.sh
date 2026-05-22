137f5f5a61cdfefeac7aaf0896b31a51cd672567
guoqing.ge <Guoqing.Ge@noaa.gov>
2026-05-21 23:25:11 -0600

    update ush/drive.sh

--- ush/drive.sh
+++ ush/drive.sh
@@ -13,21 +13,22 @@ export EXECrrfs=${EXECrrfs:-${HOMErrfs}/exec}
 export FIXrrfs=${FIXrrfs:-${HOMErrfs}/fix}
 export PARMrrfs=${PARMrrfs:-${HOMErrfs}/parm}
 export USHrrfs=${USHrrfs:-${HOMErrfs}/ush}
-export PYDAMONITOR=${HOMErrfs}/workflow/sideload/pyDAmonitor/scripts
+export PYDAMONITOR=${HOMErrfs}/workflow/sideload/pyDAmonitor
 #
 #-----------------------------------------------------------------------
 # Obtain unique process id (pid) and create the run directory (DATA).
 #-----------------------------------------------------------------------
 #
-export LOG_DIR=${COMROOT}/${NET}/${VERSION}/logs/${RUN}.${PDY}/${cyc}/${WGF}
+export MY_COM_BASE=${COMROOT}/${NET}/${VERSION}
+export LOG_DIR=${MY_COM_BASE}/logs/${RUN}.${PDY}/${cyc}/${WGF}
 if [[ "${DO_SPINUP:-FALSE}" == "TRUE" ]];  then
-  export WORKDIR=${COMOUT}/pyDAmonitor_spinup
+  export WORKDIR=${COMOUT}/pyDAmonitor_spinup/${WGF}
   export JEDI_DIR=${COMOUT}/jedivar_spinup/${WGF}
   export NONVAR_CLD_DIR=${COMOUT}/nonvar_cldana_spinup/${WGF}
   export NONVAR_BUFR_LOG=${LOG_DIR}/rrfs_nonvar_bufrobs_spinup_${TAG}_${CDATE}.log
   export NONVAR_REFL_LOG=${LOG_DIR}/rrfs_nonvar_reflobs_spinup_${TAG}_${CDATE}.log
 else
-  export WORKDIR=${COMOUT}/pyDAmonitor
+  export WORKDIR=${COMOUT}/pyDAmonitor/${WGF}
   export JEDI_DIR=${COMOUT}/jedivar/${WGF}
   export NONVAR_CLD_DIR=${COMOUT}/nonvar_cldana/${WGF}
   export NONVAR_BUFR_LOG=${LOG_DIR}/rrfs_nonvar_bufrobs_${TAG}_${CDATE}.log
@@ -38,7 +39,7 @@ cd "${WORKDIR}"
 ln -snf ${JEDI_DIR}/* .
 #
 # parse the jedi log file to get minimization.txt and obs_counts.txt
-ln -snf ${PYDAMONITOR}/parse_jedi_log.py .
+ln -snf ${PYDAMONITOR}/scripts/parse_jedi_log.py .
 ./parse_jedi_log.py
 #
 # parse the nonvar cloud analysis log files to get nonvar_cloud_out.txt
@@ -48,12 +49,19 @@ if [[ "${DO_NONVAR_CLOUD_ANA:-FALSE}" == "TRUE" ]]; then
   ln -snf ${NONVAR_BUFR_LOG} nonvar_lightning.log
   ln -snf ${NONVAR_REFL_LOG} nonvar_refmosaic.log
   ln -snf ${NONVAR_CLD_DIR}/stdout_cloudanalysis.d0000 stdout_cloudanalysis
-  ln -snf ${PYDAMONITOR}/parse_nonvar_cld_log.py .
+  ln -snf ${PYDAMONITOR}/scripts/parse_nonvar_cld_log.py .
   ./parse_nonvar_cld_log.py
 fi
 #
-ln -snf ${PYDAMONITOR}/costgrad_descent.py .
+# plot the cost/grad descent lines
+ln -snf ${PYDAMONITOR}/scripts/costgrad_descent.py .
 ./costgrad_descent.py
+#
+# plot the time series of obs counts
+WGF=''
+ln -snf ${PYDAMONITOR}/scripts/timeseries_obs_count.py .
+./timeseries_obs_count.py ${CDATE} 10  # plot 10 days of obs counts
+#
 #
 date
 echo "pyDAmonitor HAS COMPLETED NORMALLY!"
