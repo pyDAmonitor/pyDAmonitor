16d1d3521811536a064bbe0eff7faf13c30be783
Guoqing Ge <guoqing.ge@noaa.gov>
2026-05-25 19:51:48 -0600

    add yaml_factory (#39)

---
+++ ush/yaml_factory.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    echo "Usage: source ${0}"
+    exit 1
+fi
+run_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wflowdir=$(cd ${run_dir}/../..; pwd)
+cd ${run_dir}/..
+
+if [[ ! -d yaml_factory ]]; then
+  git clone https://github.com/wx-workflow/yaml_factory
+fi
+cd yaml_factory
+
+# always clone the latest rrfs-workflow and associated RDASApp
+mkdir -p external
+cd external
+rm -rf rrfs-workflow
+git clone -b rrfs-mpas-jedi https://github.com/NOAA-EMC/rrfs-workflow
+cd rrfs-workflow
+git submodule update --init sorc/RDASApp
+cd ..
+ln -snfr rrfs-workflow/parm .
+ln -snfr rrfs-workflow/sorc/RDASApp/parm/jcb-rdas .
+cd ..
+
+# create a new branch 'update'
+source ${run_dir}/load_pyDAmonitor.sh
+echo "create a new branch 'update' to keep the main branch intact"
+git checkout -b update
+git branch
+
+# updates to match the latest workflow super YAML files
+echo "updates to match the latest workflow super YAML files"
+cd factory
+./copy_from_workflow.sh
+export YT_DEDENT=fase
+export YT_SPLIT_LEVEL=1
+./yth jedivar.yaml split
+git add .
+git commit -m "updates to match the latest workflow super YAML files"
+echo -e "\ncurrent directory is: $(pwd)"
+
+# further guidance
+../ush/guide.sh
