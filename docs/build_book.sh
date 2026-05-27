1f2a50d3c7d40b0db9ffac81dc7320ac5e30abfd
Guoqing Ge <guoqing.ge@noaa.gov>
2026-05-27 01:19:22 -0600

    update build_book.sh (#41)
    * update build_book.sh
    * fine tuning

--- docs/build_book.sh
+++ docs/build_book.sh
@@ -11,21 +11,17 @@ fi
 # save the origin URL before running ghp-import, which has to use the origin remote
 save_origin=$(git remote get-url origin)
 #
-# get the remote url for current branch and set it as book_repo
-remote=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null | cut -d'/' -f1)
-if [[ -z "${remote}" ]]; then
-  echo "Push current branch to a remote first and then run build_book.sh again"
+# get the repo url of the book remote
+book_repo=$(git remote get-url book)
+if [[ -z "${book_repo}" ]]; then
+  echo "add a 'book' remote as follows"
+  echo "  git remote add book git@github.com:guoqing-noaa/mybook"
   exit 1
-fi
-book_repo=$(git remote get-url ${remote})
-#book_repo=git@github.com:pyDAmonitor/docs # use this line to overwrite the automatically-detected book_repo
-
-if [[ "${book_repo}" ==  *"NOAA-GSL/pyDAmonitor" ]] \
-   || [[ "${book_repo}" ==  *"pyDAmonitor/pyDAmonitor" ]]; then
-      echo "ERROR: current branch tracks the authoritative repository:"
-      echo "  ${book_repo}"
-      echo "users can ONLY push books to their own forks"
-      exit 1
+elif [[ "${book_repo}" ==  *"pyDAmonitor/pyDAmonitor" ]]; then
+  echo "ERROR: the 'book' remote tracks the authoritative repository:"
+  echo "  ${book_repo}"
+  echo "users can ONLY push books to their own forks"
+  exit 1
 fi
 
 set -x
@@ -42,5 +38,6 @@ git remote set-url origin ${save_origin}
 ### print out helper information
 set +x
 account=$(echo "${book_repo}" | awk -F'[:/]' '{print $(NF-1)}')
-echo -e "\nVisit https://${account}.github.io/pyDAmonitor to check the results"
+repo=$(echo "${book_repo}" | awk -F'[:/]' '{print $NF}' | sed 's/\.git$//')
+echo -e "\nVisit https://${account}.github.io/${repo} to check the results"
 echo " (Github action may take a few minutes to render the updated pages)"
