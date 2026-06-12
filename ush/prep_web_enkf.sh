f142f5c8b6bf56216693f0b261ea7cce1a12f578
guoqing.ge <Guoqing.Ge@noaa.gov>
2026-06-11 21:11:04 -0600

    add prep_web_enkf.sh

---
+++ ush/prep_web_enkf.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# prep all files needed by the webpage
+declare -rx PS4='+${SECONDS}s $(basename ${BASH_SOURCE[0]:-${FUNCNAME[0]:-"Unknown"}})[${LINENO}]${id}: '
+set -x
+
+mkdir -p web
+cd web
+sed -e "s/2024050600/${CDATE}/g" ${PYDAMONITOR}/ush/index.html_cycle_enkf > index.html
+mv ../*.png .
+mv ../*.txt .
+ln -snfr *.txt ..  # link a copy to the parent directory
+cp -L ../log.*out .
+cp -L ../getkf.yaml .
